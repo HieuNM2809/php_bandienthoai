f79440bc16703a6a2370b4b00b9d75f301046ed2
Lai Khải <56122788+LaiKhai@users.noreply.github.com>
2021-05-26 10:24:17 +0700

    update_26_05_3

--- User/layout.php
+++ User/layout.php
@@ -7,20 +7,16 @@
         require_once($level.body_path."slideshow_index.php");
         require_once($level.body_path."sanphamkhac_index.php");
         require_once($level.body_path."sansale_index.php");
-        require_once($level.footer_path."foot_index.php");
     }
     if($isMuaHang==true)
     {
         require_once($level.body_path."danhsachsanpham.php");   
         require_once($level.body_path."thuonghieu.php");
-        require_once($level.footer_path."foot_index.php");
     }
     if($isChiTietSanPham==true)
     {
         require_once($level.body_path."content_chitietsanpham.php");
         require_once($level.body_path."article_chitietsanpham.php");
-        require_once($level.footer_path."foot_chitietsanpham.php");
     }
-    
-
+    require_once($level.footer_path."foot_index.php");
 ?>
